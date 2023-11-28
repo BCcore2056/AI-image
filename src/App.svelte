2f22bf7a20ff37777788aa8f394f8207662a89a1
BCcore2056 <145463812+BCcore2056@users.noreply.github.com>
2023-11-28 14:32:48 +0000

    28.11.2023

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-  import { HfInference } from "@huggingface/inference";
+  import { HfInference } from "@huggingface/inference"; // importujemy potrzebne narzędzie z biblioteki HuggingFace
   let userInput: string;
   let imagePromise: Promise<string>;
 
-  const hf = new HfInference(import.meta.env.VITE_HUGGING_FACE_ACCESS_TOKEN);
+  const hf = new HfInference(import.meta.env.VITE_HUGGING_FACE_ACCESS_TOKEN); // konfigurujemy narzędzie z naszym kluczem dostępu
 
   async function ask() {
     const blob = await hf.textToImage({
-      model: "stabilityai/stable-diffusion-xl-base-1.0",
-      inputs: userInput,
+      // interesujący nas sposób interakcji z danym modelem - w tym wypadku wpisany tekst zamieniany na wygenerowany obraz
+      model: "stabilityai/stable-diffusion-xl-base-1.0", // konkretny model ze strony HuggingFace
+      inputs: userInput, // dane wejściowe użytkownika
     });
     return URL.createObjectURL(blob);
   }
@@ -19,16 +20,56 @@
 </script>
 
 <main>
-    <input bind:value={userInput} />
-    <button on:click={generateImage}>Generate</button>
-    {#if imagePromise}
-      {#await imagePromise}
-        Loading...
-      {:then result}
-        <!-- svelte-ignore a11y-img-redundant-alt -->
-        <img width=320 height=320 src={result} alt="Ai generated image"/>
-      {/await}
-    {:else}
-      No image generated
-    {/if}
+    <div class="centered">
+        <!-- Tutaj wpisz własny kod układu strony -->
+        <input bind:value={userInput} />
+        <button on:click={generateImage}>Generate</button>
+        {#if imagePromise}
+          {#await imagePromise}
+            <!-- tutaj oczekujemy -->
+          Loading...
+          {:then result}
+            <!-- wynik jest znany -->
+            <img width=320 height=320 src={result} alt="Ai generated"/>
+          {:catch}
+            <!-- coś się nie udało... -->
+            Oops! Failed to generate the image
+          {/await}
+        {:else}
+          No image generated
+        {/if}
+    </div>
 </main>
+
+<style>
+    /* Tu znajdzie się kod odpowiadający za wygląd strony */
+  main {
+    background-color: #555555;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+  }
+
+  input {
+    padding: 4px;
+    border: solid;
+    border-radius: 1000px;
+    text-align: center;
+  }
+
+  button {
+    padding: 4px;
+    border: solid;
+    border-radius: 1000px;
+    text-align: center;
+  }
+
+  .centered {
+    background-color: aqua;
+  }
+</style>
